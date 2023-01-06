d85a8c0c1fe981726b85bfd5234cebf7485e13ea
wymrlad <96609951+wymrlad@users.noreply.github.com>
2023-01-07 01:21:48 +0530

    Update pos-form.component.ts

--- src/app/pos-form/pos-form.component.ts
+++ src/app/pos-form/pos-form.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { CheckboxControlValueAccessor, FormControl } from '@angular/forms';
-import { type } from 'os';
 
 @Component({
   selector: 'app-pos-form',
